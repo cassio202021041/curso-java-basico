6c9c5f2e051b53faecf3dccf413f8f09fc0dd913
cassio202021041 <cassio00710@hotmail.com>
2024-06-15 17:04:04 -0300

    Commit For
    Commit For

---
+++ javabasico/javabasico/src/JavaBasicoFor.java
@@ -0,0 +1,10 @@
+public class JavaBasicoFor {
+    public static void main(String[] args) {
+        
+        for(int count = 1; count <= 100; count++){
+            System.out.println("Numero implementado count: " + count);
+
+        }
+    }
+
+}
