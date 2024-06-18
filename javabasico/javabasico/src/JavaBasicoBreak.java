378e997709fcd8c3f838296c410f8dbf4d8fba50
cassio202021041 <cassio00710@hotmail.com>
2024-06-18 10:50:55 -0300

    Commit break
    Commit Break

---
+++ javabasico/javabasico/src/JavaBasicoBreak.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class JavaBasicoBreak {
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.println("Entre com um numero: ");
+        int num = scan.nextInt();
+
+        System.out.println("Entre com o limite maximo: ");
+        int max = scan.nextInt();
+
+        for (int i=num; i<=max; i++){
+            //System.out.println(i);  
+
+            if (i % 7 == 0){
+
+                System.out.println("O valor de I é: " + i);
+            
+                break;//Para o for no primeito divisor de 7
+            }
+
+           
+        }
+    }
+
+}
